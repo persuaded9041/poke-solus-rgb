3cf2e65bfd43f93f7c70f4e4717168b0f719c55c
Derek Andersen <derekcandersen@gmail.com>
2024-09-02 21:09:46 -0400

    add 3 new maps to town map entries, at the bottom because this seems to not harm anything

--- data/maps/town_map_entries.asm
+++ data/maps/town_map_entries.asm
@@ -106,8 +106,7 @@ InternalMapEntries:
 	internal_map VERMILION_TRADE_HOUSE,         10,  9, VermilionCityName
 	internal_map DIGLETTS_CAVE,                  3,  4, DiglettsCaveName
 	internal_map VICTORY_ROAD_3F,                0,  4, VictoryRoadName
-	;internal_map UNUSED_MAP_CE,                  7,  5, RocketHQName ;removed this entry in order to fit TRADER_HOUSE (below) in the table
-	internal_map TRADER_HOUSE,                   7,  5, CeladonCityName
+	internal_map UNUSED_MAP_CE,                  7,  5, RocketHQName
 	internal_map SILPH_CO_8F,                   10,  5, SilphCoName
 	internal_map POKEMON_MANSION_B1F,            2, 15, PokemonMansionName
 	internal_map SAFARI_ZONE_NORTH_REST_HOUSE,   8, 12, SafariZoneName
@@ -116,5 +115,8 @@ InternalMapEntries:
 	internal_map CERULEAN_BADGE_HOUSE,          10,  2, CeruleanCityName
 	internal_map ROCK_TUNNEL_B1F,               14,  3, RockTunnelName
 	internal_map SILPH_CO_ELEVATOR,             10,  5, SilphCoName
+	internal_map MEW_ROOM,                       2, 15, PokemonMansionName ; dereknote - new map
+	internal_map FOSSIL_ROOM,                    0,  4, VictoryRoadName ; dereknote - new map
+	internal_map TRADER_HOUSE,                   7,  5, CeladonCityName ; dereknote - new map
 	internal_map AGATHAS_ROOM,                   0,  2, PokemonLeagueName
 	db -1 ; end
